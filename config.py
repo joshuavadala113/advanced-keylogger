274d445f80f4c1fc4c570f8788fa06c43c3da691
joshuavadala113 <154101307+joshuavadala113@users.noreply.github.com>
2025-06-04 12:39:17 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # config.py
 
 # Email Configuration
-EMAIL_FROM = "your_email@gmail.com"
-EMAIL_PASSWORD = "your_app_password"  # Use Gmail app password or equivalent
-EMAIL_TO = "your_email@gmail.com"     # Receiver's email address
+EMAIL_FROM = "joshuavadalal@gmail.com"
+EMAIL_PASSWORD = "yebg dtgk yrvw dhog"  # Use Gmail app password or equivalent
+EMAIL_TO = "joshuavadala@gmail.com"     # Receiver's email address
 
 # Encryption Key (paste the key you generated here)
 ENCRYPTION_KEY = b'xl1ZT5MX0mwk6WSMLnwXsoDgmdtgyCtgA9KhNDMfP90='
